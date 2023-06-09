200b1f37f0e3fe8b92b706a06feeae20d9487d81
GeenDrive <66233204+GeneDrive@users.noreply.github.com>
2023-06-09 13:28:39 +0200

    added get customer by country

--- functionality/QuantityType.cs
+++ functionality/QuantityType.cs
@@ -3,3 +3,9 @@ public class QuantityType
     public int id {get; set;}
     public int quantity {get; set;}
 }
+
+public class CountryQuantityType
+{
+    public string country {get; set;}
+    public int quantity {get; set;}
+}
